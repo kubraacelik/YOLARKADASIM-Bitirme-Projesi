1a93fcd3db85d69fb373603af2c9421b48ba72d1
kubraacelik <kkbra.celik92@gmail.com>
2024-05-08 22:11:31 +0300

    eklendi

--- yolarkadasim/src/components/Tarih.jsx
+++ yolarkadasim/src/components/Tarih.jsx
@@ -1,29 +1,28 @@
 import React, { useState } from "react";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { LocalizationProvider, StaticDatePicker, DatePicker } from "@mui/x-date-pickers";
+import { LocalizationProvider, StaticDatePicker } from "@mui/x-date-pickers";
 import { Stack } from "@mui/material";
+import dayjs from "dayjs"; // dayjs kütüphanesini import edin
 
 export const Tarih = ({ handleTarihChange }) => {
-  const [selectedDate, setSelectedDate] = useState(null); // Boş değer durumu için null olarak başlatıyoruz
-  
+  const [selectedDate, setSelectedDate] = useState(null);
+
+  // Tarihi doğru formata dönüştürmek için dayjs kütüphanesini kullanın
   function formatDate(date) {
-    const day = date.date()+1;
-    const month = date.month()+1;
-    const year = date.year();
-    return `${year}-${month.toString().padStart(2, "0")}-${day.toString().padStart(2, "0")}`;
+    return dayjs(date).format("YYYY-MM-DD");
   }
-  
+
   const handleDateChange = (date) => {
     const formattedDate = formatDate(date);
-    setSelectedDate(date); // Tarih seçildiğinde state'i güncelle
-    handleTarihChange(formattedDate); // Seçilen tarihi ana bileşene ilet
+    setSelectedDate(date);
+    handleTarihChange(formattedDate);
   };
 
   return (
     <Stack sx={{ border: '3px solid black', borderRadius: '20px', padding: '10px'}}>
       <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="tr">
         <StaticDatePicker
-          value={selectedDate} // Seçilen tarihi belirle
+          value={selectedDate}
           onChange={handleDateChange}
           disablePast
         />
