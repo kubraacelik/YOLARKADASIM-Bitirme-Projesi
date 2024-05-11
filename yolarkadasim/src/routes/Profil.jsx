9239f525cad407a862b021bd0c2e1e32c2792027
kubraacelik <kkbra.celik92@gmail.com>
2024-05-11 21:11:12 +0300

    değişiklik yapıldı

--- yolarkadasim/src/routes/Profil.jsx
+++ yolarkadasim/src/routes/Profil.jsx
@@ -3,8 +3,15 @@ import "../styles/Profil.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Avatar from "../assets/avatar.jpg";
+import { jwtDecode } from "jwt-decode";
 
 export const Profil = () => {
+  const userToken = localStorage.getItem('token');
+  const data = jwtDecode(userToken);
+  const name = data.user.ad;
+  const surname = data.user.soyad;
+  const password = data.user.sifre;
+  const eposta = data.user.eposta;
   return (
     <>
       <Navbar />
@@ -16,27 +23,22 @@ export const Profil = () => {
         </div>
         <div className="profilBilgi">
           <label className="profil-label">Ad</label>
-          <p className="profil-paragraf">İremnur</p>
+          <p className="profil-paragraf">{name}</p>
           <button className="btn">Düzenle</button>
         </div>
         <div className="profilBilgi">
           <label className="profil-label">Soyad</label>
-          <p className="profil-paragraf">Ahi</p>
+          <p className="profil-paragraf">{surname}</p>
           <button className="btn">Düzenle</button>
         </div>
         <div className="profilBilgi">
           <label className="profil-label">Şifre</label>
-          <p className="profil-paragraf">Zonguldak</p>
+          <p className="profil-paragraf">{password}</p>
           <button className="btn">Düzenle</button>
         </div>
         <div className="profilBilgi">
           <label className="profil-label">Email</label>
-          <p className="profil-paragraf">iremnurahi@gmail.com</p>
-          <button className="btn">Düzenle</button>
-        </div>
-        <div className="profilBilgi">
-          <label className="profil-label">Doğum Günü</label>
-          <p className="profil-paragraf">30/01/2000</p>
+          <p className="profil-paragraf">{eposta}</p>
           <button className="btn">Düzenle</button>
         </div>
       </div>
