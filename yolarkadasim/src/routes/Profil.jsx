a5d1376d7451ccb4247d89738b7992473bf3039a
kubraacelik <kkbra.celik92@gmail.com>
2024-05-13 09:53:56 +0300

    Profile ekleme yapıldı

--- yolarkadasim/src/routes/Profil.jsx
+++ yolarkadasim/src/routes/Profil.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/Profil.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
@@ -8,6 +8,7 @@ import { styled } from "@mui/material/styles";
 import Button from "@mui/material/Button";
 import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 import { TextField } from "@mui/material";
+import { InputText } from "primereact/inputtext";
 
 export const Profil = () => {
   const userToken = localStorage.getItem("token");
@@ -17,8 +18,12 @@ export const Profil = () => {
   const password = data.sifre;
   const eposta = data.eposta;
 
-  const [profileImage, setProfileImage] = useState(Avatar);
-  const [tempProfileImage, setTempProfileImage] = useState(null); // Geçici profil resmi
+  const [profileImage, setProfileImage] = useState(
+    localStorage.getItem("profileImage") || Avatar
+  );
+  const [tempProfileImage, setTempProfileImage] = useState(
+    localStorage.getItem("tempProfileImage") || null
+  );
 
   const handleImageChange = (e) => {
     const file = e.target.files[0];
@@ -26,11 +31,17 @@ export const Profil = () => {
     reader.onloadend = () => {
       setProfileImage(reader.result);
       setTempProfileImage(reader.result); // Geçici profil resmini güncelle
+      localStorage.setItem("tempProfileImage", reader.result); // Geçici profil resmini localStorage'e kaydet
     };
     if (file) {
       reader.readAsDataURL(file);
     }
   };
+
+  useEffect(() => {
+    // Component yüklendiğinde, localStorage'den geçici profil resmini al
+    localStorage.setItem("profileImage", profileImage);
+  }, [profileImage]);
 
   const VisuallyHiddenInput = styled("input")({
     clip: "rect(0 0 0 0)",
@@ -48,85 +59,82 @@ export const Profil = () => {
     <>
       <Navbar />
       <div className="enKapsayıcı">
-      <div className="profil">Profilinizi Düzenleyin</div>
+        <div className="profil">Profilinizi Düzenleyin</div>
         <div className="kapsayıcı">
           <div className="kapsayıcıSol">
-            <img className="kişiResmi" src={tempProfileImage || profileImage} alt="" />
+            <img
+              className="kişiResmi"
+              src={tempProfileImage || profileImage}
+              alt=""
+            />
             <div className="kapsayıcıBtnUpload">
-              <Button
+              <InputText
+                type="file"
+                accept="/image/*"
+                onChange={handleImageChange}
                 className="btnUpload"
-                component="label"
-                role={undefined}
-                variant="contained"
-                tabIndex={-1}
-                startIcon={<CloudUploadIcon />}
-              >
-                Resmi Güncelle
-                <VisuallyHiddenInput type="file" onChange={handleImageChange} />
-              </Button>
+              />
             </div>
           </div>
 
           <div className="kapsayıcıSag">
             <div className="kapsayıcıSagBolum">
-            <div className="profilBilgileri">
-              <div className="profilBilgi">
-                <label className="profil-label">Ad</label>
-                <TextField
-                  className="profil-paragraf"
-                  id="outlined-basic"
-                  label={name}
-                  variant="outlined"
-                  disabled
-                  InputLabelProps={{
-                    style: {
-                      fontSize: "20px",
-                      fontWeight: "normal",
-                      color: "black",
-                    },
-                  }}
-                />
+              <div className="profilBilgileri">
+                <div className="profilBilgi">
+                  <label className="profil-label">Ad :</label>
+                  <TextField
+                    className="profil-paragraf"
+                    id="outlined-basic"
+                    label={name}
+                    variant="outlined"
+                    disabled
+                    InputLabelProps={{
+                      style: {
+                        fontSize: "20px",
+                        fontWeight: "normal",
+                        color: "black",
+                      },
+                    }}
+                  />
+                </div>
+                <div className="profilBilgi">
+                  <label className="profil-label">Soyad :</label>
+                  <TextField
+                    className="profil-paragraf"
+                    id="outlined-basic"
+                    label={surname}
+                    variant="outlined"
+                    disabled
+                    InputLabelProps={{
+                      style: {
+                        fontSize: "20px",
+                        fontWeight: "normal",
+                        color: "black",
+                      },
+                    }}
+                  />
+                </div>
+                <div className="profilBilgi">
+                  <label className="profil-label">E-mail :</label>
+                  <TextField
+                    className="profil-paragraf"
+                    id="outlined-basic"
+                    label={eposta}
+                    variant="outlined"
+                    disabled
+                    InputLabelProps={{
+                      style: {
+                        fontSize: "20px",
+                        fontWeight: "normal",
+                        color: "black",
+                      },
+                    }}
+                  />
+                </div>
               </div>
-              <div className="profilBilgi">
-                <label className="profil-label">Soyad</label>
-                <TextField
-                  className="profil-paragraf"
-                  id="outlined-basic"
-                  label={surname}
-                  variant="outlined"
-                  disabled
-                  InputLabelProps={{
-                    style: {
-                      fontSize: "20px",
-                      fontWeight: "normal",
-                      color: "black",
-                    },
-                  }}
-                />
+              <div className="yeniSifre">
+                <button className="yeniSifreBtn">Şifreni Güncelle</button>
               </div>
-              <div className="profilBilgi">
-                <label className="profil-label">E-mail</label>
-                <TextField
-                  className="profil-paragraf"
-                  id="outlined-basic"
-                  label={eposta}
-                  variant="outlined"
-                  disabled
-                  InputLabelProps={{
-                    style: {
-                      fontSize: "20px",
-                      fontWeight: "normal",
-                      color: "black",
-                    },
-                  }}
-                />
-              </div>
-            </div>
-            <div className="yeniSifre">
-              <button className="yeniSifreBtn">
-                Şifreni Güncelle
-              </button>
-            </div>
             </div>
           </div>
         </div>
